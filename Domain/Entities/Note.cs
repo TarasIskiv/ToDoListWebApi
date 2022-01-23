611ff75eb0f8f3ff5f5f39558c92e62fd7fc55b2
Taras <taras.iskiv@gmail.com>
2022-01-23 17:54:11 +0100

    Added Domain realization

---
+++ Domain/Entities/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastModified { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ Domain/Interfaces/INoteRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface INoteRepository
+    {
+        public void AddNewNote(Note note);
+        public void DeleteNote(Note note);
+        public IEnumerable<Note> GetAllNotes();
+        public Note GetNoteById(int id);
+        public void Update(Note note);
+
+    }
+}
