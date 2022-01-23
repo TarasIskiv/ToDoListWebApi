95695b48e6adb67c9d97525d570ef5ee9afcdad8
Taras <taras.iskiv@gmail.com>
2022-01-23 18:04:06 +0100

    Added default Infrastructure realization. Added needed Nuget packages.

---
+++ Infrastructure/Repositories/NoteRepository.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        public void AddNewNote(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteNote(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Note> GetAllNotes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Note GetNoteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Note note)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
